be6f47260529cf5efb16a5c0698fc07e7b35b940
Matéo Pasquier <pasquiermateo33@gmail.com>
2024-12-16 12:15:21 +0100

    Added the xyz input file.

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 import("fmt")
 
+/*
+   Name: Matéo Pasquier
+   Email: mateo.pasquier@ens.uvsq.fr
+   Course Section: ISM
+   Instructions to build the program: go build main.go
+   Instructions to run the program: ./main
+*/
+
 func main() {
 	fmt.Println("skibidi");
 }
